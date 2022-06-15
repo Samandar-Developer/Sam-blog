6a665f1367fdaa087ede784587f265ecbc2792b7
Madaliev Samandar <samandarmadaliev@yandex.com>
2022-06-15 14:27:43 +0500

    create models in blog app and create file path for media files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import uuid 
 
-# Create your models here.
+
+class Blog(models.Model):
+    # owner
+    title = models.CharField(max_length=200) 
+    cover_image = models.ImageField(default="default.jpg", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+    date = models.DateField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Comment(models.Model):
+    GOOD="Good"
+    BAD="Bad"
+    EMOJI_TYPE = (
+        (GOOD, "👍"),
+        (BAD, "👎")
+    )
+    # owner 
+    blog = models.ForeignKey("Blog", on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    react = models.CharField(max_length=100, choices=EMOJI_TYPE, default=GOOD, null=True, blank=True)
+
+    def __str__(self):
+        return self.react
+    
